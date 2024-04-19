03dfd45d59ea77fc7ad17a8bacb0e4b6908afcd9
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-04-19 17:47:57 +0530

    Add files via upload

---
+++ React Components 1/NFTium/sol/src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { Component } from "react";
+
+import Navbar from "./Navbar.js";
+import Hero from "./Hero.js";
+import About from "./About.js";
+
+export default function App() {
+  // Write this code in Navbar.js file
+ 
+
+  // Write this code in Hero.js file
+  
+
+  // Write this code in About.js file
+  
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
