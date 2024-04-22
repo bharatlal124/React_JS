39963cc79213edf02a6afac4397c5324ae9fa37d
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-04-22 21:17:04 +0530

    Add files via upload

---
+++ React Components 1/User Profile/src/Skills.js
@@ -0,0 +1,20 @@
+// Create Skill component here to display your skills
+// In this component there should be a list with each listitem as your skill.
+
+import React from "react";
+
+class Skills extends React.Component {
+  render() {
+    return (
+      <>
+        <ul>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+      </>
+    );
+  }
+}
+export default Skills;
