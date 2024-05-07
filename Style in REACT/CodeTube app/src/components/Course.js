7509dc33cbc9bc416ce78888411fe476cc5cee9e
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-05-07 18:43:33 +0530

    Add files via upload

---
+++ Style in REACT/CodeTube app/src/components/Course.js
@@ -0,0 +1,36 @@
+// ./src/components/Course.js
+import { Component } from "react";
+// import Button from "../resuable.styled";
+import {Button} from "../reusable.styled.js";
+
+class Video extends Component {
+  render() {
+    const {
+      id,
+      title,
+      description,
+      image,
+      instructor,
+      price
+    } = this.props.video;
+
+    const { onAdd, onRemove, isInBag } = this.props;
+
+    return (
+      <div className="course">
+        <img src={image} alt={title} />
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <p>{instructor}</p>
+          <strong>{price}</strong>
+          <Button bg="red" onClick={() => (!isInBag ? onAdd(id) : onRemove(id))}>
+            {!isInBag ? "Add to bag" : "Remove from bag"}
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Video;
