1fe7005ccaaa2d60148f99bada32828e5e9737ca
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-05-13 15:47:55 +0530

    Add files via upload

---
+++ Style in REACT/Scoping Styles with CSS modules/src/List.js
@@ -0,0 +1,17 @@
+// import "./list.css";
+import listcss from "./list.module.css";
+import Item from "./components/Item";
+import { data } from "./data";
+
+export default function List() {
+  return (
+    <>
+      <h3 className={listcss.title}>Items Listed for Sale</h3>
+      <div className={listcss.container}>
+        {data.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </div>
+    </>
+  );
+}
