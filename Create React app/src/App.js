30d6be8b1d5c0d4d4d1b9cf780da1c77ef076588
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-11-28 10:18:54 +0530

    Add files via upload

---
+++ Create React app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {
+        /* Write the message here. */
+        "Welcome to React Course on codingninjas.com"
+      }
+    </div>
+  );
+}
+
+export default App;
